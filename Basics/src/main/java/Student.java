20a4b365d46cc03994562e7584fe30d78273403a
Sushant Kumar <sushant.k052@gmail.com>
2026-08-13 10:53:58 +0530

    Initial class

---
+++ Basics/src/main/java/Student.java
@@ -0,0 +1,56 @@
+public class Student {
+
+    /* States */
+    String id;
+    String name;
+    int division;
+    char section;
+
+    /* Constructor */
+
+    public Student(String id, String name, int division, char section) {
+        this.id = id;
+        this.name = name;
+        this.division = division;
+        this.section = section;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    private void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDivision() {
+        return division;
+    }
+
+    private void setDivision(int division) {
+        this.division = division;
+    }
+
+    public char getSection() {
+        return section;
+    }
+
+    private void setSection(char section) {
+        this.section = section;
+    }
+
+
+    public void read()
+    {
+        System.out.println(name+ " is reading ");
+    }
+}
---
+++ Basics/src/main/java/StudentMain.java
@@ -0,0 +1,10 @@
+public class StudentMain {
+
+    public static void main(String[] args) {
+
+        Student student = new Student("1","Sushant",10,'A');
+
+        student.read();
+
+    }
+}
