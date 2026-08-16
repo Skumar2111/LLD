a03c31b77c4fbbf054e9d1dd8e3bb22614ff1f41
Sushant Kumar <sushant.k052@gmail.com>
2026-08-16 09:55:05 +0530

    Designing Builder Pattern

---
+++ Basics/src/main/java/DesignPattern/Creational/Builder/Builder.java
@@ -0,0 +1,46 @@
+package DesignPattern.Creational.Builder;
+
+public class Builder {
+
+    protected int id;
+    protected String name;
+    protected String address;
+
+    public static Builder newInstance()
+    {
+        return new Builder();
+    }
+
+    public Builder setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public Builder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Builder setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    private Builder(){};
+
+
+    public Student build()
+    {
+        return new Student(this);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Builder{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
---
+++ Basics/src/main/java/DesignPattern/Creational/Builder/Main.java
@@ -0,0 +1,11 @@
+package DesignPattern.Creational.Builder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        StudentReceiver receiver = new StudentReceiver();
+        System.out.println(receiver.getStudent());
+    }
+
+
+}
